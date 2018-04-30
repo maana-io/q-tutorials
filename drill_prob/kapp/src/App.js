dd7878c6c4e9175903003edb957777c08c166e83
witt3rd <witt3rd@gmail.com>
2018-04-30 13:09:25 -0700

    update to match training

--- drill_prob/kapp/src/App.js
+++ drill_prob/kapp/src/App.js
@@ -7,37 +7,35 @@ import gql from "graphql-tag";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { place: "Reading, UK", result: "text" };
+    this.state = { lat: 1.0, long: 2.0, result: "(no result)" };
   }
 
-  handleChange = event => {
-    this.setState({ place: event.target.value });
+  handleChange = (event, key) => {
+    this.setState({ [key]: event.target.value });
   };
 
   handleSubmit = event => {
+    console.log("event", event);
     this.props.client
       .query({
         query: gql`
-          query GeoLocationQuery($place: String!) {
-            geoLocation(place: $place) {
-              city
-              country
-              mapLink
-              coords
-              weather {
-                summary
-                temperature
-              }
+          query LocationExpDrillProb($lat: Float!, $long: Float!) {
+            locationExpDrillProb(lat: $lat, long: $long) {
+              Problem
+              Probability
             }
           }
         `,
         variables: {
-          place: this.state.place
+          lat: this.state.lat,
+          long: this.state.long
         }
       })
-      .then(response =>
-        this.setState({ result: JSON.stringify(response.data.geoLocation) })
-      );
+      .then(response => {
+        this.setState({
+          result: JSON.stringify(response.data.locationExpDrillProb)
+        });
+      });
 
     event.preventDefault();
   };
@@ -47,16 +45,24 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Drilling Problems</h1>
+          <h1 className="App-title">Accenture: Drilling Problems</h1>
         </header>
         <p className="App-intro">Candidate Location</p>
         <form onSubmit={this.handleSubmit}>
           <label>
-            Place:
+            Lat:
             <input
               type="text"
-              value={this.state.place}
-              onChange={this.handleChange}
+              value={this.state.lat}
+              onChange={event => this.handleChange(event, "lat")}
+            />
+          </label>
+          <label>
+            Long:
+            <input
+              type="text"
+              value={this.state.long}
+              onChange={event => this.handleChange(event, "long")}
             />
           </label>
           <input type="submit" value="Submit" />
