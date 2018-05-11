deaca8990a99665be94addeba597eec6df97f1ef
donald <donald@maana.io>
2018-05-11 04:58:59 -0700

    add simple test

---
+++ cli/test/test00.sh
@@ -0,0 +1,4 @@
+gql mload -p test00 -c 00/employer.csv -m addEmployers
+gql mload -p test00 -j 00/employer.json -m addEmployers
+gql mload -p test00 -c 00/person.csv -m addPersons
+gql mload -p test00 -j 00/person.json -m addPersons
