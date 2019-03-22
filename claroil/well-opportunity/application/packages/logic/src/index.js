73ab7fd3f11ba79fdcdd820013281cca83fa22df
Roie Cohen <roie.cohen@gmail.com>
2019-03-22 00:53:17 -0700

    mocks

--- claroil/well-opportunity/application/packages/logic/src/index.js
+++ claroil/well-opportunity/application/packages/logic/src/index.js
@@ -1,6 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
-const { MockList } = require('graphql-tools')
 const faker = require('faker')
 
 const resolvers = {
@@ -9,17 +8,21 @@ const resolvers = {
       name: 'ClairOil Well Optimization Demo',
       version: '0.0.1'
     }),
-    wellPredictatedRate(parent, { well }) {
+
+    //TODO
+    wellPredictatedMetrics(parent, { well }) {
       return {
-        id: 'bla',
+        id: 'some_id',
         waterCut: Math.random() * 10,
         GOR: Math.random() * 10,
         oilRate: Math.random() * 10
       }
     },
-    wellMeasuredRate(parent, { well }) {
+
+    //TODO
+    wellMeasuredMetrics(parent, { well }) {
       return {
-        id: 'bla',
+        id: 'some_id',
         waterCut: Math.random() * 10,
         GOR: Math.random() * 10,
         oilRate: Math.random() * 10
@@ -30,39 +33,78 @@ const resolvers = {
       return Math.random() * 10
     },
 
-    discoverIntervention(parent, { predictedRate, measuredRate }) {
-      return {
-        id: 'hey',
-        action: {
+    discoverIntervention(parent, { predictedMetrics, measuredMetrics }) {
+      //Watercut
+      let predictedWatercut = predictedMetrics.waterCut
+      let measuredWatercut = measuredMetrics.waterCut
+      let waterCutGap = measuredWatercut - predictedWatercut
+
+      //OilRate
+      let predictedOilRate = predictedMetrics.oilRate
+      let measuredOilRate = measuredMetrics.oilRate
+
+      let oilRateGap =
+        (100 * (predictedOilRate - measuredOilRate)) / predictedOilRate
+
+      let action = {}
+
+      if (waterCutGap > 0.07) {
+        action = {
           id: 'action',
-          name: 'actionName',
-          type: 'costSaving'
-        },
+          name: 'WATER_SHUT_OFF',
+          type: 'REVENUE_GAIN'
+        }
+      }
+      if (oilRateGap > 0.08) {
+        action = {
+          id: 'action',
+          name: 'HYDRAULIC_FRACTURING',
+          type: 'REVENUE_GAIN'
+        }
+      }
+      if (oilRateGap > 0.05 && oilRateGap < 0.08) {
+        action = {
+          id: 'action',
+          name: 'ACIDIZING',
+          type: 'REVENUE_GAIN'
+        }
+      }
+
+      return {
+        id: 'some_id',
+        action,
         probability: 0.9
       }
     },
 
     shouldTestWell(parent, { healthIndex, lastTestDate }) {
-      return {
-        id: 'hey',
-        action: {
-          id: 'action',
-          name: 'actionName',
-          type: 'costSaving'
-        },
-        probability: 0.8
-      }
+      let today = '03/03/2019'
+      let testGap = 30
+
+      let defaultAnomaly = 0.2 //TODO: Pass this value in
+      let shouldTestConfidence =
+        healthIndex >= 0.8
+          ? true
+          : healthIndex >= 0.5 && healthIndex < 0.8
+          ? defaultAnomaly
+          : 1
+      shouldTestConfidence = testGap > 60 ? 1 : shouldTestConfidence
+
+      return shouldTestConfidence
     },
 
+    //TODO
     getWellLastTestDate(parent, { well }) {
       return '02/02/2019'
     },
+
+    //TODO
     applyConstraints(parent, { opportunities, constraints }) {
       return [
         {
-          id: faker.random.uuid(),
+          id: 'some_id',
           name: 'op',
-          createAt: faker.date.recent(),
+          createdAt: '02/02/2019',
           actions: [
             {
               id: 'action',
@@ -76,12 +118,12 @@ const resolvers = {
             }
           ],
           incrementalRevenue: Math.random() * 10000,
-          costReductions: Math.random() * 1000
+          costReduction: Math.random() * 1000
         },
         {
-          id: faker.random.uuid(),
+          id: 'some_id',
           name: 'op',
-          createAt: faker.date.recent(),
+          createdAt: '02/02/2019',
           actions: [
             {
               id: 'action',
@@ -95,16 +137,20 @@ const resolvers = {
             }
           ],
           incrementalRevenue: Math.random() * 10000,
-          costReductions: Math.random() * 1000
+          costReduction: Math.random() * 1000
         }
       ]
     },
 
-    combineActionImpacts(parent, { costReductions, revenueGains }) {
+    //TODO
+    combineActionImpacts(parent, { costReduction, revenueGains }) {
+      let incrementalRevenue = 0
+      let costReduction = 0
+
       return {
-        id: faker.random.uuid(),
+        id: 'some_id',
         name: 'op',
-        createAt: faker.date.recent(),
+        createdAt: '02/02/2019',
         actions: [
           {
             id: 'action',
@@ -118,7 +164,7 @@ const resolvers = {
           }
         ],
         incrementalRevenue: Math.random() * 10000,
-        costReductions: Math.random() * 1000
+        costReduction: Math.random() * 1000
       }
     },
 
@@ -126,31 +172,63 @@ const resolvers = {
       parent,
       { measuredMetrics, probabilityOfAnomaly, oilPrice }
     ) {
-      return Math.random() * 1000
+      let costOfSkippikingATest =
+        measuredMetrics.oilRate * probabilityOfAnomaly * oilPrice * 60
+      return costOfSkippikingATest
     },
 
-    calculateInterventionRevenueGain(parent, { well, action, cost }) {
+    calculateInterventionRevenueGain(
+      parent,
+      { oilPrice, interventionCost, measuredMetrics, action }
+    ) {
+      let rateOfIncrease = 0.5
+      let revenueIncrease =
+        measuredMetrics.oilRate * oilPrice * 180 * rateOfIncrease
+
       return {
-        id: 'hey',
-        action: {
-          id: 'action',
-          name: 'actionName',
-          type: 'costSaving'
-        },
-        impact: Math.random() * 10000,
-        cost: Math.random() * 10000
+        id: 'some_id',
+        action,
+        impact: revenueIncrease,
+        cost: interventionCost
       }
     },
+
     calculateHealthIndex(parent, { predictedMetrics, measuredMetrics }) {
-      return Math.random() * 10
+      //Watercut
+      let predictedWatercut = predictedMetrics.waterCut
+      let measuredWatercut = measuredMetrics.waterCut
+      let waterCutHealthIndex =
+        1 - Math.abs((predictedWatercut - measuredWatercut) / predictedWatercut)
+
+      //GOR
+      let predictedGOR = predictedMetrics.GOR
+      let measuredGOR = measuredMetrics.GOR
+      let GORHealthIndex =
+        1 - Math.abs((predictedGOR - measuredGOR) / predictedGOR)
+
+      //OilRate
+      let predictedOilRate = predictedMetrics.oilRate
+      let measuredOilRate = measuredMetrics.oilRate
+      let oilRateHealthIndex =
+        1 - Math.abs((predictedOilRate - measuredOilRate) / predictedOilRate)
+
+      return (
+        (1 / 3) * waterCutHealthIndex +
+        (1 / 3) * GORHealthIndex +
+        (1 / 3) * oilRateHealthIndex
+      )
+    },
+
+    calculateInterventionCost(parent, { well, action }) {
+      return 5000
     },
 
     projectAction(parent, { actionProbability }) {
       let { action } = actionProbability
-      console.log('action', action)
-      action.id = 'bla'
+      action.id = 'some_id'
       return action
     },
+
     projectProbability(parent, { actionProbability }) {
       let { probability } = actionProbability
       return probability
@@ -158,25 +236,26 @@ const resolvers = {
 
     wrapActionFinancialEstimate(parent, { actionEstimate }) {
       let { action } = actionEstimate
-      action.id = 'bla'
+      action.id = 'some_id'
       actionEstimate.action = action
       return [actionEstimate]
     },
 
     makeFinancialEstimate(parent, { action, impact, cost }) {
       return {
-        id: 'hey',
-        action: {
-          id: 'action',
-          name: 'actionName',
-          type: 'costSaving'
-        },
-        impact: Math.random() * 10000,
-        cost: Math.random() * 10000
+        id: 'some_id',
+        action,
+        impact,
+        cost
       }
     },
+
     invertFloat(parent, { value }) {
       return value * -1
+    },
+
+    getCurrentOilPrice() {
+      return Math.random() * 100
     }
   }
 }
