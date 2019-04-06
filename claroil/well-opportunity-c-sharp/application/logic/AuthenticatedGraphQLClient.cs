fdb9d3f8a1e1f4b83582c7b4c0faa3d0163fe88f
Logan Gore <lgore@maana.io>
2019-04-05 18:03:56 -0700

    Synchonize singleton--remove request logging middleware from last accidentaly commit.

--- claroil/well-opportunity-c-sharp/application/logic/AuthenticatedGraphQLClient.cs
+++ claroil/well-opportunity-c-sharp/application/logic/AuthenticatedGraphQLClient.cs
@@ -1,47 +1,9 @@
 using System;
 using GraphQL.Client;
 using Maana.OAuth;
-using System.Net.Http;
-using System.Threading.Tasks;
-using System.Threading;
 
 namespace Maana.AuthenticatedGraphQLClient
 {
-
-    public class LoggingHandler : DelegatingHandler
-    {
-        public LoggingHandler(HttpMessageHandler innerHandler)
-            : base(innerHandler)
-        {
-        }
-
-        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            var rand = new Random().Next();
-
-            var time1 = DateTime.Now.ToString("ss-ffff");
-
-            Console.WriteLine("OUTGOING Request: "+rand+"   "+time1+"  "+request.ToString());
-            if (request.Content != null)
-            {
-                Console.WriteLine(await request.Content.ReadAsStringAsync());
-            }
-            Console.WriteLine();
-
-            HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
-            var time2 = DateTime.Now.ToString("ss-ffff");
-
-            Console.WriteLine("INCOMING Reponse: "+rand+"   "+time2+"  "+response.ToString());
-            if (response.Content != null)
-            {
-                Console.WriteLine(await response.Content.ReadAsStringAsync());
-            }
-            Console.WriteLine();
-
-            return response;
-        }
-    }
-
     public class Client
     {
         private Client()
@@ -64,13 +26,7 @@ namespace Maana.AuthenticatedGraphQLClient
                     if(String.IsNullOrWhiteSpace(ksvcsURI))
                         throw new Exception ("REMOTE_KSVC_ENDPOINT_URL environment variable is null/empty.");
 
-                    // Add options with custom loggin middleware
-                    var options = new GraphQLClientOptions();
-                    var httpClientHandler = new HttpClientHandler();
-                    Console.WriteLine(httpClientHandler.MaxConnectionsPerServer);
-                    options.HttpMessageHandler = new LoggingHandler(httpClientHandler);
-
-                    client = new GraphQLClient(ksvcsURI, options);
+                    client = new GraphQLClient(ksvcsURI);
 
                     // Pass names of environment variables to OAuth fetcher to get credentials.
                     var authFetcher = OAuthFetcher.CreateFetcherUsingEnvironmentVariableNames(
