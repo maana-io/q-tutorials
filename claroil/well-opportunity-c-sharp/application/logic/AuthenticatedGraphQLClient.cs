8a8867edd460f96e2874afc12da53b1fc4e35d83
Logan Gore <lgore@maana.io>
2019-04-05 17:58:59 -0700

    Synchonize singleton.

--- claroil/well-opportunity-c-sharp/application/logic/AuthenticatedGraphQLClient.cs
+++ claroil/well-opportunity-c-sharp/application/logic/AuthenticatedGraphQLClient.cs
@@ -1,8 +1,47 @@
 using System;
 using GraphQL.Client;
 using Maana.OAuth;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+
 namespace Maana.AuthenticatedGraphQLClient
 {
+
+    public class LoggingHandler : DelegatingHandler
+    {
+        public LoggingHandler(HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var rand = new Random().Next();
+
+            var time1 = DateTime.Now.ToString("ss-ffff");
+
+            Console.WriteLine("OUTGOING Request: "+rand+"   "+time1+"  "+request.ToString());
+            if (request.Content != null)
+            {
+                Console.WriteLine(await request.Content.ReadAsStringAsync());
+            }
+            Console.WriteLine();
+
+            HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
+            var time2 = DateTime.Now.ToString("ss-ffff");
+
+            Console.WriteLine("INCOMING Reponse: "+rand+"   "+time2+"  "+response.ToString());
+            if (response.Content != null)
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+            Console.WriteLine();
+
+            return response;
+        }
+    }
+
     public class Client
     {
         private Client()
@@ -10,39 +49,49 @@ namespace Maana.AuthenticatedGraphQLClient
 
         }
         static GraphQLClient client;
+
+        static object locker = new object();
+
         public static GraphQLClient GetClientInstance()
         {
-            if(client == null)
-            {
-                Console.WriteLine("{ AuthenticatedGraphQLClient } Creating authenticated graphql client.");
+            lock(locker){
+                if(client == null)
+                {
+                    Console.WriteLine("{ AuthenticatedGraphQLClient } Creating authenticated graphql client.");
 
-                string ksvcsURI = Environment.GetEnvironmentVariable("REMOTE_KSVC_ENDPOINT_URL");
+                    string ksvcsURI = Environment.GetEnvironmentVariable("REMOTE_KSVC_ENDPOINT_URL");
 
-                if(String.IsNullOrWhiteSpace(ksvcsURI))
-                    throw new Exception ("REMOTE_KSVC_ENDPOINT_URL environment variable is null/empty.");
+                    if(String.IsNullOrWhiteSpace(ksvcsURI))
+                        throw new Exception ("REMOTE_KSVC_ENDPOINT_URL environment variable is null/empty.");
 
-                client = new GraphQLClient(ksvcsURI); 
+                    // Add options with custom loggin middleware
+                    var options = new GraphQLClientOptions();
+                    var httpClientHandler = new HttpClientHandler();
+                    Console.WriteLine(httpClientHandler.MaxConnectionsPerServer);
+                    options.HttpMessageHandler = new LoggingHandler(httpClientHandler);
 
-                // Pass names of environment variables to OAuth fetcher to get credentials.
-                var authFetcher = OAuthFetcher.CreateFetcherUsingEnvironmentVariableNames(
-                    "MACHINE_TO_MACHINE_APP_AUTH_DOMAIN",
-                    "MACHINE_TO_MACHINE_APP_AUTH_CLIENT_ID",
-                    "MACHINE_TO_MACHINE_APP_AUTH_CLIENT_SECRET",
-                    "MACHINE_TO_MACHINE_APP_AUTH_IDENTIFIER");
+                    client = new GraphQLClient(ksvcsURI, options);
 
-                var oauth = authFetcher.GetOAuthToken();
+                    // Pass names of environment variables to OAuth fetcher to get credentials.
+                    var authFetcher = OAuthFetcher.CreateFetcherUsingEnvironmentVariableNames(
+                        "MACHINE_TO_MACHINE_APP_AUTH_DOMAIN",
+                        "MACHINE_TO_MACHINE_APP_AUTH_CLIENT_ID",
+                        "MACHINE_TO_MACHINE_APP_AUTH_CLIENT_SECRET",
+                        "MACHINE_TO_MACHINE_APP_AUTH_IDENTIFIER");
 
-                if(oauth!=null){
-                    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {oauth.AccessToken??""}");
+                    var oauth = authFetcher.GetOAuthToken();
+
+                    if(oauth!=null){
+                        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {oauth.AccessToken??""}");
+                    }
+
+                    return client;
                 }
-
-                return client;
+                else
+                {
+                    return client;
+                }
             }
-            else
-            {
-                return client;
-            }
-
         }
     }
 }
